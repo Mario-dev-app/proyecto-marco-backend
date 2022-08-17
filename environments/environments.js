2a907ad4ec7d5d5ac850df130a9792667d1e487f
Mario <marioperaltadev@gmail.com>
2022-08-17 16:10:56 -0500

    Se agregó cadena de conexión

---
+++ environments/environments.js
@@ -0,0 +1,7 @@
+const params = {
+    serverNode: '172.16.1.52:30015',
+    uid: 'system',
+    pwd: 'AtTu2AUC'
+};
+
+module.exports = params;
--- routes/socios-de-negocio.js
+++ routes/socios-de-negocio.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const hana = require('@sap/hana-client');
+const connection = hana.createConnection();
+const params = require('../environments/environments');
 
-app.get('/', (req, res) => {
-    res.json({
-        ok: 'true'
-    });
+
+app.get('/api', (req, res) => {
+    connection.connect(params, (err) => {
+        if (err) throw err;
+
+        connection.exec('SELECT * FROM "SBO_MARCO_PE"."ODLN" WHERE "DocEntry" = ?', [46617], (err, rows) => {
+            if(err) throw err;
+            res.json({
+                ok: true,
+                data: rows
+            });
+            connection.disconnect();
+        });
+    })
 });
 
 module.exports = app;
